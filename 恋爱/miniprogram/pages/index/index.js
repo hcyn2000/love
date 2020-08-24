a018130da17efc8928629abbea9abeca6e6b5415
duanxin1234 <863351709@qq.com>
2020-08-24 22:15:36 +0800

    love

--- 恋爱/miniprogram/pages/index/index.js
+++ 恋爱/miniprogram/pages/index/index.js
@@ -5,7 +5,18 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    value: '',
+  },
+  onChange(e) {
+    this.setData({
+      value: e.detail,
+    });
+  },
+  onSearch() {
+    Toast('搜索' + this.data.value);
+  },
+  onClick() {
+    Toast('搜索' + this.data.value);
   },
 
   /**
